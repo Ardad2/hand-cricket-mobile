9a02ad2635e80b4167199837538e2ff80661cc12
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2021-12-12 23:30:18 -0700

    input validation

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Text, StyleSheet, TextInput, Button} from 'react-native';
 import Card from '../components/Card';
 import Input from '../components/input';
 import Colors from '../constants/colors';
 
 const StartGameScreen = props => {
+
+    const [enteredValue, setEnteredValue] = useState('');
+
+    const numberInputHandler = inputText => {
+        setEnteredValue(inputText.replace(/[^0-6]/g, ''));
+    };
+
     return (
         <View style={styles.screen}>
             <Text style={styles.title}>Start !</Text>
          <Card style={styles.inputContainer}>
              <Text>Select a Number!</Text>
-             <Input style={styles.Input}/>
+             <Input style={styles.input} 
+             blurOnSubmit 
+             autoCaptalize='none' 
+             autoCorrect='false' 
+             keyboardType="number-pad" 
+             maxLength={1}
+             onChangeText={numberInputHandler}
+             value={enteredValue}/>
              <View style={styles.buttonContainer}>
                  <Button title = "Reset" onPress={() => {}} color={Colors.accent}/>
                  <Button title = "Confirm" onPress={() => {}} color={Colors.primary}/>
